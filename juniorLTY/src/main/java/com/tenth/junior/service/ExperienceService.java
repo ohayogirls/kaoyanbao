2e5ee71ead79cb13bfb98d661ae48e66caaadc1b
douglaslee <2205459936@qq.com>
2018-06-26 17:21:15 +0800

    material & experience

--- juniorLTY/src/main/java/com/tenth/junior/service/ExperienceService.java
+++ juniorLTY/src/main/java/com/tenth/junior/service/ExperienceService.java
@@ -1,4 +1,25 @@
 package com.tenth.junior.service;
 
+import com.tenth.junior.bean.Experience;
+import com.tenth.junior.bean.Material;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public interface ExperienceService {
+    List<Experience> getAllExperience();
+
+    Experience addExperience(Experience experience);
+
+    Optional<Experience> findByID(Integer ID);
+
+    Experience UpdateExperience(Experience experience);
+
+    void deleteExperience(Experience experience);
+
+    List<Experience> getAllExp();
+
+    List<Experience> queryByExperienceType(String type);
+
 }
--- juniorLTY/src/main/java/com/tenth/junior/service/ExperienceServiceImpl.java
+++ juniorLTY/src/main/java/com/tenth/junior/service/ExperienceServiceImpl.java
@@ -1,4 +1,53 @@
 package com.tenth.junior.service;
 
-public class ExperienceServiceImpl {
+
+import com.tenth.junior.bean.Experience;
+import com.tenth.junior.repository.ExperienceRepository;
+import com.tenth.junior.repository.MaterialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ExperienceServiceImpl implements  ExperienceService{
+
+    @Autowired
+    private ExperienceRepository experiencerepository;
+
+    @Override
+    public List<Experience> getAllExperience() {
+        return experiencerepository.findAll();
+    }
+
+    @Override
+    public Experience addExperience(Experience experience) {
+        return experiencerepository.save(experience);
+    }
+
+    @Override
+    public Optional<Experience> findByID(Integer ID) {
+        return experiencerepository.findById(ID);
+    }
+
+    @Override
+    public Experience UpdateExperience(Experience experience) {
+        return experiencerepository.save(experience);
+    }
+
+    @Override
+    public void deleteExperience(Experience experience) {
+        experiencerepository.delete(experience);
+    }
+
+    @Override
+    public List<Experience> getAllExp() {
+        return null;
+    }
+
+    @Override
+    public List<Experience> queryByExperienceType(String type) {
+        return experiencerepository.queryByType(type);
+    }
 }
