2a132b8c1b6971ec8eca311d9a30cab7679a8e41
douglaslee <2205459936@qq.com>
2018-06-26 17:05:17 +0800

    material

--- juniorLTY/src/main/java/com/tenth/junior/service/MaterialService.java
+++ juniorLTY/src/main/java/com/tenth/junior/service/MaterialService.java
@@ -1,4 +1,24 @@
 package com.tenth.junior.service;
 
+import com.tenth.junior.bean.Material;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public interface MaterialService {
+    List<Material> getAllMaterial();
+
+    Material addMaterial(Material material);
+
+    Optional<Material> findByID(Integer ID);
+
+    Material UpdateMaterial(Material material);
+
+    void deleteMaterial(Material material);
+
+    List<Material> getAllMat();
+
+    List<Material> queryByMaterialType(String type);
+
 }
--- juniorLTY/src/main/java/com/tenth/junior/service/MaterialServiceImpl.java
+++ juniorLTY/src/main/java/com/tenth/junior/service/MaterialServiceImpl.java
@@ -1,4 +1,50 @@
 package com.tenth.junior.service;
 
-public class MaterialServiceImpl {
+import com.tenth.junior.bean.Material;
+import com.tenth.junior.repository.MaterialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class MaterialServiceImpl implements  MaterialService{
+    @Autowired
+    private MaterialRepository materialrepository;
+
+    @Override
+    public List<Material> getAllMaterial(){
+        return materialrepository.findAll();
+    }
+    @Override
+    public Material addMaterial(Material material) {
+        return materialrepository.save(material);
+    }
+
+    @Override
+    public Optional<Material> findByID(Integer ID) {
+        return materialrepository.findById(ID);
+    }
+
+    @Override
+    public Material UpdateMaterial(Material material) {
+        return materialrepository.save(material);
+    }
+
+    @Override
+    public void deleteMaterial(Material material) {
+        materialrepository.delete(material);
+    }
+
+    @Override
+    public List<Material> getAllMat() {
+        return null;
+    }
+
+    @Override
+    public List<Material> queryByMaterialType(String type) {
+        return materialrepository.queryByType(type);
+    }
+
+
 }
