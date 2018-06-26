ad37aeb192c4f7e1998fbd2919b1358e22ab24df
Sherlock <530317349@qq.com>
2018-06-26 18:04:28 +0800

    完成yearlydata增删改查

--- juniorLTY/src/main/java/com/tenth/junior/repository/YearlyDataRepository.java
+++ juniorLTY/src/main/java/com/tenth/junior/repository/YearlyDataRepository.java
@@ -3,6 +3,11 @@ package com.tenth.junior.repository;
 import com.tenth.junior.bean.YearlyData;
 import com.tenth.junior.bean.YearlyDataPK;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface YearlyDataRepository extends JpaRepository<YearlyData,YearlyDataPK>{
+    @Query("select a from YearlyData a where a.schoolID = ?1")
+    public List<YearlyData> queryBySchoolID(Integer schoolid);
 }
--- juniorLTY/src/main/java/com/tenth/junior/service/YearlyDataServiceImpl.java
+++ juniorLTY/src/main/java/com/tenth/junior/service/YearlyDataServiceImpl.java
@@ -1,4 +1,45 @@
 package com.tenth.junior.service;
 
-public class YearlyDataServiceImpl {
+import com.tenth.junior.bean.YearlyData;
+import com.tenth.junior.bean.YearlyDataPK;
+import com.tenth.junior.repository.YearlyDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class YearlyDataServiceImpl implements YearlyDataService{
+    @Autowired
+    private YearlyDataRepository yearlyDataRepository;
+
+    @Override
+    public List<YearlyData> findAllYearlyData() {
+        return yearlyDataRepository.findAll();
+    }
+
+    @Override
+    public YearlyData addYearlyData(YearlyData yearlyData) {
+        return yearlyDataRepository.save(yearlyData);
+    }
+
+    @Override
+    public YearlyData updateYearlyData(YearlyData yearlyData) {
+        return yearlyDataRepository.save(yearlyData);
+    }
+
+    @Override
+    public Optional<YearlyData> findByID(YearlyDataPK pk) {
+        return yearlyDataRepository.findById(pk);
+    }
+
+    @Override
+    public void deleteYearlyData(YearlyDataPK pk) {
+        yearlyDataRepository.deleteById(pk);
+    }
+
+    @Override
+    public List<YearlyData> findBySchoolID(Integer id){
+        return yearlyDataRepository.queryBySchoolID(id);
+    }
+
 }
