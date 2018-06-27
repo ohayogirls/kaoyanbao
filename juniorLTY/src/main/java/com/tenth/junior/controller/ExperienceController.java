09171b434511c1b4f9cd1be4faa0b4d5e502fd14
douglaslee <2205459936@qq.com>
2018-06-27 17:14:21 +0800

    conntroller of EXP

--- juniorLTY/src/main/java/com/tenth/junior/controller/ExperienceController.java
+++ juniorLTY/src/main/java/com/tenth/junior/controller/ExperienceController.java
@@ -1,4 +1,88 @@
 package com.tenth.junior.controller;
 
+
+import com.tenth.junior.bean.Experience;
+import com.tenth.junior.bean.School;
+import com.tenth.junior.service.ExperienceService;
+import com.tenth.junior.service.SchoolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/experience")
 public class ExperienceController {
+    @Autowired
+    private ExperienceService experienceService;
+    //@Autowired
+    //private SchoolService schoolService;
+
+    /**
+     * 查看所有的经验贴
+     *此时应该需要返回到当前界面
+     */
+    @GetMapping
+    public String index(Model model){
+        List<Experience> list =experienceService.getAllExperience();
+        model.addAttribute("exp",list);
+        return "experience-table";
+    }
+
+
+    /**
+     * 添加一条经验
+     * 添加界面：experience/add
+     */
+    @PostMapping("/add")
+    public String input(Experience experience){
+       // Optional<School> school;
+        //school=schoolService.getExperienceByID(experience.getSchoolID());
+        experienceService.addExperience(experience);
+        return "redirect:/";
+    }
+    /**
+     * 显示添加界面
+     * 展示所有的学校,,,！！！！！这里的schoolService没有写好，请在合并后修改
+     */
+    @GetMapping("/add")
+    public String addPage(Model model){
+        //model.addAttribute("allSchool",schoolService.getAll());//待修改
+        return "addExp";
+    }
+
+    /**
+     * 删除经验功能
+     *
+     */
+    @GetMapping("/delete/{id}")
+    public String deleteExp(@PathVariable("id") Integer id){
+        Optional<Experience> experience=experienceService.findByID(id);
+        experienceService.deleteExperience(experience.get());
+        return "redirect:/";
+    }
+
+    /**修改功能
+     *
+     */
+    @PostMapping("/update")
+    public String updateData(Experience experience){
+//        Optional<Experience> experience1 = experienceService.get (student.getGrade().getGID()); ;
+//        student.setGrade(grade.get());
+        //experience.setSchoolID();
+         experienceService.addExperience(experience);
+        return "redirect:/";
+    }
+    /**
+     *
+     */
+
+
 }
