6094da7c25329d241f0ad674b23a1fcb0f384fdb
DolittleQZ <3329231051@qq.com>
2018-06-28 10:39:34 +0800

    logintable & user v3.0

--- juniorLTY/src/main/java/com/tenth/junior/repository/UserRepository.java
+++ juniorLTY/src/main/java/com/tenth/junior/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.tenth.junior.repository;
 
 import com.tenth.junior.bean.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
+    @Query("select a from User a where a.username =?1 and password = ?2")
+    User getUserByNamAndPassword(String username, String password);
+
+    @Query("select count(bean.id) from User bean")
+    public long getUserNumber();
 }
