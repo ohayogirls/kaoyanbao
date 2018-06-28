828b7ad5e4762afef918e79c6e8eb18787fc15a2
lc-xie <stephen@hust.edu.cn>
2018-06-28 09:16:09 +0800

    SchoolController commit by xlc 1.0

--- juniorLTY/src/main/java/com/tenth/junior/controller/SchoolController.java
+++ juniorLTY/src/main/java/com/tenth/junior/controller/SchoolController.java
@@ -1,17 +1,29 @@
 package com.tenth.junior.controller;
 
+import com.tenth.junior.bean.School;
+import com.tenth.junior.service.SchoolService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/")
-public class SchoolController {
-    @GetMapping
-    public String index(Model model){
+import java.util.List;
 
+@Controller
+@RequestMapping("/school")
+public class SchoolController {
+
+    @Autowired
+    private SchoolService schoolService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        List<School> schoolList = schoolService.findAllSchool();
+        model.addAttribute("school",schoolList);
         return "school-table";
     }
+
+
 
 }
